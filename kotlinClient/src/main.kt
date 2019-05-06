48cd7ca1c22de351a3ebb831398e76527f78d1d8
Eric <java_specialist_ma@yahoo.com>
2019-05-06 01:10:26 -0400

    Read server using Socket.

--- kotlinClient/src/main.kt
+++ kotlinClient/src/main.kt
@@ -1,3 +1,25 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.net.Socket
+
+    fun main() {
+
+        val socket = Socket("localhost", 51366)
+        var buf = ""
+        var len = 0
+
+        try {
+            val outStream = DataOutputStream(socket.getOutputStream())
+            outStream.writeUTF("Hello Pison.");
+            outStream.flush();
+
+            val inStream = DataInputStream(socket.getInputStream())
+            buf = inStream.readUTF()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            println(len)
+            println(buf)
+            socket.close()
+        }
+    }
