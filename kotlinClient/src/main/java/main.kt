f1088d8efac0de3e42c2ebae6b977a85d9684d55
Eric <java_specialist_ma@yahoo.com>
2019-05-07 01:30:29 -0400

    Parse frame

--- kotlinClient/src/main/java/main.kt
+++ kotlinClient/src/main/java/main.kt
@@ -1,16 +1,27 @@
 import java.io.InputStreamReader
 import java.net.Socket
+import org.json.JSONObject
 import kotlin.system.exitProcess
 
 fun main() {
+    println("Please enter the port number...")
+
     val port = readLine()!!.toInt()
     val socket = Socket("localhost", port)
     val maxLines = 20
     var count = 0
 
-    try {
+    class Frame(json: String) : JSONObject(json) {
+        val timeStamp: String? = this.optString("timeStamp")
+        val data:      String? = this.optString("data")
+        val label:     String? = this.optString("label")
+    }
+
+        try {
             InputStreamReader(socket.getInputStream()).forEachLine {
-                println(it)
+//                println(it)
+                val frame = Frame(it)
+                println(">>>>   " + frame.timeStamp + " " + frame.data)
                 if (count++ > maxLines) exitProcess(0)
             }
 
