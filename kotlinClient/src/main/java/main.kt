31b55d3248323e02317e9d9208245ec84d650c97
Eric Christiansen <t.e.christiansen@gmail.com>
2019-05-08 02:10:08 -0400

    Cleanup.

--- kotlinClient/src/main/java/main.kt
+++ kotlinClient/src/main/java/main.kt
@@ -1,59 +1,73 @@
 import java.io.InputStreamReader
-import java.net.Socket
 import org.json.JSONObject
 import java.io.File
-import java.io.OutputStreamWriter
 import kotlin.system.exitProcess
 
 fun main() {
     // println("Please enter the port number...")
 
     // val notification = "Activation classified\n"
-    val maxFrames    = 200
-    val minPercent   = 25
-    val maxNoise     = 2
+    val maxFrames  = 200
+    val minPercent = 25
+    val maxNoise   = 2
 
     //val port       = readLine()!!.toInt()
     //val socket     = Socket("localhost", port)
-    var numFrames  = 0
-    var maxData    = Int.MIN_VALUE
-    var minData    = Int.MAX_VALUE
+    var numFrames = 0
+    var maxData = Int.MIN_VALUE
+    var minData = Int.MAX_VALUE
 
-    class Frame(json: String): JSONObject(json) {
+    class Frame(json: String) : JSONObject(json) {
         val timeStamp: String? = this.optString("timeStamp")
-        val data:      String? = this.optString("data")
-        val label:     String? = this.optString("label")
+        val data: String? = this.optString("data")
+        val label: String? = this.optString("label")
     }
 
 
     var lastData = 0
     var lastLast: Int
 
-    fun deriv (data: Int): Pair<Int,Int> {
+    fun deriv(data: Int): Pair<Int, Int> {
         val delta = data - lastData
-        lastLast  = lastData
-        lastData  = data
-        return Pair(delta, (data - 2*lastData - lastLast))
+        lastLast = lastData
+        lastData = data
+        return Pair(delta, (data - 2 * lastData - lastLast))
     }
 
+
+    var range:       Int
+    val validRange = 85000
 
     fun percent(intData: Int): Int {
-        if (maxData < intData) maxData = intData
-        if (minData > intData) minData = intData
+        maxData = Math.max(intData, maxData)
+        minData = Math.min(intData, minData)
+        range   = Math.max((maxData - minData), 1)
+        if (range < validRange) return 0
 
-        if (maxData != minData) return 100 * (intData - minData) / (maxData - minData)
-
-        return 0
+        return 100 * (intData - minData) / range
     }
 
 
-    fun activationFound(intData: Int, d1: Int, d2: Int) = percent(intData) > 25
-        //(intData > 0) && (d1 > 5000) && (d2 < -10000)
+    var numNoise = maxNoise
+    var percent  = 0
+
+    fun activationFound(intData: Int, d1: Int, d2: Int): Boolean {
+        percent   = percent(intData)
+        var found = (percent > minPercent)
+        // (intData > 0) && (d1 > 5000) && (d2 < -10000)
+
+        if (found) numNoise = 0
+        else numNoise++
+
+        found = (numNoise < maxNoise) || found
+
+        return found
+    }
 
     try {
         //val writer = OutputStreamWriter(socket.getOutputStream())
         val inputstream = File("some.json").inputStream()
-        //val inputstream2 = socket.getInputStream()
+        //val inputstream = socket.getInputStream()
 
         InputStreamReader(inputstream).forEachLine {
             val frame   = Frame(it)
@@ -61,11 +75,6 @@ fun main() {
 
             var flag = ""
             val (d1, d2) = deriv(intData)
-
-            if (maxData < intData) { maxData = intData }
-            if (minData > intData) { minData = intData }
-            var percent = 50
-            if (maxData != minData) percent = 100 * (intData - minData) / (maxData - minData)
 
             if (activationFound(intData, d1, d2)) {
                 //writer.write(notification)
